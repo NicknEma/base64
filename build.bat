caf933f09f9ca950f35be3807ccad53290eb9cb7
NicknEma <emanuele.rovini@gmail.com>
2025-05-21 23:43:25 +0200

    Fix file name to match repo description

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 del *.pdb > NUL 2> NUL
 del *.rdi > NUL 2> NUL
-cl base64.c       -nologo -Fe:base64.exe       -Z7 -link -incremental:no -opt:ref
-cl base64_tests.c -nologo -Fe:base64_tests.exe -Z7 -link -incremental:no -opt:ref
+cl base64.c             -nologo -Fe:base64.exe             -Z7 -link -incremental:no -opt:ref
+cl base64_experiments.c -nologo -Fe:base64_experiments.exe -Z7 -link -incremental:no -opt:ref
 del *.ilk > NUL 2> NUL
 del *.obj > NUL 2> NUL
